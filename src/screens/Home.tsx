c9918c4dd5578c505a30baa3506e7ecf6a259f0d
marcossaro <marcossarocobo@gmail.com>
2023-03-22 22:03:33 +0100

    Initial commit

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,140 @@
-import React from 'react'
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { useEffect, useState } from 'react'
+import { Button, Text, ScrollView, TouchableOpacity, View, ActivityIndicator, Alert } from 'react-native/types'
+import { PROPERTIES_ENDPOINT } from '../utils/endpoints';
 
-const Home = () => <></>
+type RootStackParamList = {
+    Login: undefined;
+    Home: { token: string }
+};
 
+type PropertiesProps = {
+    title: string;
+    properties: Array<string>
+};
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
+
+const Home = ({ navigation, route }: Props) => {
+    const [properties, setProperties] = useState<PropertiesProps | null>(null);
+
+    const [page, setPage] = useState(1);
+    const [size, setSize] = useState(50);
+  
+    const chargeProperties = async () => {
+      try {
+        const result = await fetch(PROPERTIES_ENDPOINT, {
+          method: "POST",
+          headers: {
+            Authentication: `Bearer ${route.params.token}`,
+          },
+          body: JSON.stringify({
+            page: page,
+            per_page: size,
+          }),
+        });
+  
+        if (result.status === 200) {
+          const value = result.json();
+          const newValue = Object.keys(value).map((e) => {
+            if (value[e].length > 0) {
+              return {
+                title: e.charAt(0).toUpperCase(),
+                properties: value[e],
+              };
+            }
+          })
+          setProperties(newValue);
+        } else {
+          Alert.alert('ERROR', '', [
+            {
+              text: 'Cerrar',
+              style: 'cancel',
+            },
+          ]);
+        }
+      } catch (error) {
+        Alert.alert('ERROR', '', [
+            {
+              text: 'Cerrar',
+              style: 'cancel',
+            },
+        ]);
+      }
+    };
+  
+    useEffect(() => {
+      chargeProperties();
+    }, [page, size]);
+  
+    if (!properties) {
+      <View
+        style={{
+          flex: 1,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <ActivityIndicator />
+      </View>;
+    } else {
+      return (
+        <View
+          style={{
+            flex: 1,
+            display: "flex",
+            padding: 20,
+          }}
+        >
+          <ScrollView style={{ flex: 1, width: "100%" }}>
+            {properties && properties.length > 0 && properties
+              .sort(
+                (a: any, b: any) =>
+                  a.title.toLowerCase().charCodeAt(0) -
+                  b.title.toLowerCase().charCodeAt(0)
+              )
+              .map((item: any, index: number) => {
+                return (
+                  <TouchableOpacity
+                    key={index}
+                    onPress={() =>
+                      setProperties((prevState) => prevState.splice(index, 1))
+                    }
+                  >
+                    <View style={{ padding: 5 }}>
+                      <Text style={{ fontSize: 14, fontWeight: "bold" }}>
+                        {item.title}
+                      </Text>
+                      {item.properties.sort().map((p: string, i: number) => (
+                        <Text
+                          key={i}
+                          style={{ fontSize: 12, fontWeight: "400", padding: 10 }}
+                        >
+                          {p}
+                        </Text>
+                      ))}
+                    </View>
+                  </TouchableOpacity>
+                );
+              })}
+          </ScrollView>
+          <View style={{ display: "flex", flexDirection: "row", width: "100%" }}>
+            <Button
+              onPress={() => setPage((prevState) => prevState - 1)}
+              disabled={page === 1}
+              title="Anterior"
+              color="#009066"
+            />
+            <Button
+              onPress={() => setPage((prevState) => prevState + 1)}
+              title="Siguiente"
+              color="#009066"
+            />
+          </View>
+        </View>
+      );
+    }
+  }  
 
 export default Home
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,6 +1,171 @@
-import React from 'react'
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { useState } from 'react'
+import { Button, Dimensions, Image, StyleSheet, Text, TextInput, TouchableOpacity, View, Alert } from 'react-native/types'
+import { LOGIN_ENDPOINT } from '../utils/endpoints';
 
-const Login = () => <></>
+type RootStackParamList = {
+    Login: undefined;
+    Home: { token: string }
+};
 
+type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
+
+const Login = ({ navigation }: Props) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [visible, setVisible] = useState(false);
+  
+    const submit = async () => {
+      try {
+        const result = await fetch(LOGIN_ENDPOINT, {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        });
+  
+        if (result.status === 200) {
+          const value = result.json();
+          navigation.navigate("Home", { token: value as string });
+        } else {
+            Alert.alert('ERROR', 'Email o contraseña incorrecto.', [
+                {
+                  text: 'Cerrar',
+                  style: 'cancel',
+                },
+            ]);
+        }
+      } catch (error) {
+        Alert.alert('ERROR', 'Email o contraseña incorrecto.', [
+            {
+              text: 'Cerrar',
+              style: 'cancel',
+            },
+        ]);
+      }
+    };
+  
+    return (
+      <View
+        style={{
+          flex: 1,
+          display: "flex",
+          padding: 20,
+        }}
+      >
+        <Image
+          style={{
+            width: Dimensions.get("window").width * 0.8,
+            height: 150,
+            alignSelf: "center",
+          }}
+          source={require("./assets/logo.png")}
+          resizeMode={"contain"}
+        />
+        <Text style={{ fontSize: 24, fontWeight: "bold" }}>Iniciar sesión</Text>
+        <View style={{ marginVertical: 10 }}>
+          <Text style={{ fontSize: 12, fontWeight: "500", marginBottom: 5 }}>
+            Correo electrónico
+          </Text>
+  
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              borderRadius: 6,
+              borderWidth: 1,
+              borderColor: "rgba(0, 0, 0, 0.5)",
+              width: "100%",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              style={styles.icon}
+              source={require("../../assets/email.png")}
+              resizeMode={"contain"}
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={setEmail}
+              value={email}
+              autoCorrect={false}
+              placeholder={"juan@correo.com"}
+            />
+          </View>
+        </View>
+  
+        <View style={{ marginBottom: 20 }}>
+          <Text style={{ fontSize: 12, fontWeight: "500", marginBottom: 5 }}>
+            Contraseña
+          </Text>
+  
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              borderRadius: 6,
+              borderWidth: 1,
+              borderColor: "rgba(0, 0, 0, 0.5)",
+              width: "100%",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              style={styles.icon}
+              source={require("../../assets/password.png")}
+              resizeMode={"contain"}
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={setPassword}
+              value={password}
+              autoCorrect={false}
+              secureTextEntry={!visible}
+              placeholder={"********"}
+            />
+            <TouchableOpacity
+              onPress={() => setVisible((prevState) => !prevState)}
+            >
+              <Image
+                style={styles.icon}
+                source={
+                  visible
+                    ? require("../../assets/showPassword.png")
+                    : require("../../assets/hidePassword.png")
+                }
+                resizeMode={"contain"}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+  
+        <Button
+          onPress={submit}
+          title="Iniciar sesión"
+          color="#009066"
+        />
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      borderWidth: 0,
+      flex: 1,
+    },
+    icon: {
+      width: 20,
+      height: 20,
+      marginHorizontal: 10,
+      tintColor: "rgba(0, 0, 0, 0.5)",
+    },
+  });
+  
 
 export default Login
