418a460d4f4e67c36103d113263b83dd6d23c2a5
Friosn <franz.rios.nino@gmail.com>
2023-03-27 20:45:07 +0200

    getProperties function service

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -1,4 +1,5 @@
 import { LoginResponse } from "./interfaces";
+import { PropertyResponse } from "./interfaces";
 import { Alert } from 'react-native';
 
 
@@ -21,6 +22,31 @@ export async function login(deviceName: string, email: string, password: string)
           
         } else {
             //Maybe in a more complex example I would return an error instead an alert to separate UI layer and server layer
+          Alert.alert('Error', 'Login failed. Please check your credentials.');
+          return null
+        }
+      } catch (error) {
+        Alert.alert('Error', 'An error occurred while logging in.')
+        return null
+      }
+}
+
+export async function getProperties(token:string): Promise<PropertyResponse | null> {
+    try {
+        const response = await fetch('https://dev.homming.com/api/manager/properties', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          }
+        });
+  
+        if (response.ok) {
+          const data = await response.json() as PropertyResponse;
+          return data
+          
+        } else {
+        
           Alert.alert('Error', 'Login failed. Please check your credentials.');
           return null
         }
