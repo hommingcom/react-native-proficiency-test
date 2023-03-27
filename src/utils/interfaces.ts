bd5fba378d62ce9a80d45bdddbc402304e526fed
Friosn <franz.rios.nino@gmail.com>
2023-03-27 18:35:46 +0200

    Properties Interface

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -27,3 +27,72 @@ export interface LoginResponse {
   message: string
 
 }
+
+export interface Property {
+  id: number;
+  owner_id: number;
+  team_id: number | null;
+  crm_id: number | null;
+  type: string;
+  name: string;
+  address: string | null;
+  address_details: string | null;
+  address_fields: string | null;
+  latitude: number | null;
+  longitude: number | null;
+  bathrooms_count: number | null;
+  rooms_count: number | null;
+  room_management: boolean;
+  reference: string | null;
+  register_number: string | null;
+  last_alteration: string | null;
+  last_check_in: string | null;
+  fee_amount: number | null;
+  fee_percentage: number | null;
+  purchase_price: number | null;
+  refurbishment_price: number | null;
+  taxes: number | null;
+  register_valuation: number | null;
+  management_fee: number | null;
+  notary: number | null;
+  management: number | null;
+  current_valuation: number | null;
+  state_valuation: number | null;
+  cadastral_valuation: number | null;
+  rental_price_index: number | null;
+  constructed_area: string;
+  usable_area: string | null;
+  extras: string | null;
+  comments: string | null;
+  cleaning_expense: number | null;
+  created_at: string;
+  updated_at: string;
+  common_area: string | null;
+  active_contract_tenants_count: number;
+  occupied_rooms_count: number;
+  collections: string[];
+  media: any[]; // You can replace `any` with the appropriate type if you have more information about the media structure
+  tags: any[]; // You can replace `any` with the appropriate type if you have more information about the tags structure
+}
+
+export interface PaginationLink {
+  url: string | null;
+  label: string;
+  active: boolean;
+}
+
+export interface PropertyResponse {
+  current_page: number;
+  data: Property[];
+  first_page_url: string;
+  from: number;
+  last_page: number;
+  last_page_url: string;
+  links: PaginationLink[];
+  next_page_url: string | null;
+  path: string;
+  per_page: number;
+  prev_page_url: string | null;
+  to: number;
+  total: number;
+}
